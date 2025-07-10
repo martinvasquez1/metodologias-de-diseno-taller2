2410dfc6d1c348640c6366dd685a88b830c45880
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-07-09 20:40:31 -0400

    Remover health check
    No funcionaba correctamente

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 # app/main.py
 import logging
 from fastapi import FastAPI
-from pyctuator.pyctuator import Pyctuator
 import uvicorn
 import os
 
 from app.infrastructure.http_handlers.payment_handler import (
     router_payments,
 )  # Importar el nuevo router
-from app.infrastructure.http_handlers.handler_health_check import router_health
 
 # Configuración básica de logging si no está en log.py de forma global
 logging.basicConfig(level=logging.INFO)
@@ -19,19 +17,8 @@ app = FastAPI(
     version="1.0.0",
 )
 
-# Integrar Pyctuator para monitoreo (opcional, pero útil para health check)
-pyctuator = Pyctuator(
-    app=app,
-    app_name="PayTrackApp",  # Nombre de tu aplicación
-    app_url="http://localhost:8000",  # URL donde se ejecutará tu API
-    registration_url=None,  # URL para el registro
-    pyctuator_endpoint_url="/pyctuator",  # Este ya lo tenías, lo mantenemos
-)
-
-
 # Registrar routers
 app.include_router(router_payments)
-app.include_router(router_health)
 
 
 # Puedes añadir un endpoint raíz simple para verificar que la app corre
