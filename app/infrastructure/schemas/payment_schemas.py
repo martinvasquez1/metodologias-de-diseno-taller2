b39e66794972609a9d6e336aba12bc34a2cd83b5
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-07-09 20:45:54 -0400

    Remover comentarios para http handlers dto

--- app/infrastructure/schemas/payment_schemas.py
+++ app/infrastructure/schemas/payment_schemas.py
@@ -1,4 +1,3 @@
-# app/infrastructure/schemas/payment_schemas.py
 from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Optional, List
@@ -6,13 +5,11 @@ from typing import Optional, List
 from app.core.entities.payment import PaymentStatus
 
 
-# RF1: Entradas para Registrar un Pago [cite: 22]
 class PaymentCreateInput(BaseModel):
     nombre_cliente: str = Field(..., example="Juan Perez")
-    monto: float = Field(..., example=150.75)  # RF1: Monto > 0 [cite: 26]
+    monto: float = Field(..., example=150.75)
 
 
-# Salida para Pagos (RF1, RF2, RF3)
 class PaymentOutput(BaseModel):
     id: int = Field(..., example=1)
     nombre_cliente: str = Field(..., example="Juan Perez")
