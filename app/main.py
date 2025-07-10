b26eab2a88af2983561bd108f8f089ca63a30554
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-07-09 20:49:31 -0400

    Remover comentarios main

--- app/main.py
+++ app/main.py
@@ -1,30 +1,24 @@
-# app/main.py
 import logging
 from fastapi import FastAPI
 import uvicorn
 import os
 
-from app.infrastructure.http_handlers.payment_handler import (
-    router_payments,
-)  # Importar el nuevo router
+from app.infrastructure.http_handlers.payment_handler import router_payments
 
-# Configuración básica de logging si no está en log.py de forma global
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI(
     title="PayTrack API",
-    description="API para el registro, gestión y consulta de pagos realizados por clientes.",
+    description="API para gestión de pagos",
     version="1.0.0",
 )
 
-# Registrar routers
 app.include_router(router_payments)
 
 
-# Puedes añadir un endpoint raíz simple para verificar que la app corre
 @app.get("/")
 async def root():
-    return {"message": "PayTrack API is running!"}
+    return {"message": "PayTrack en ejecución"}
 
 
 if __name__ == "__main__":
